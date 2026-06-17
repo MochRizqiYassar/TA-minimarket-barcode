e4477dc0e780519254c6e8c4b86d1d5e7f9f5ce0
MochRizqiYassar <kikioryassar.2003@gmail.com>
2026-06-17 10:29:35 +0700

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,30 @@
-const CACHE_NAME = 'inventori-cache-v1';
+const CACHE_NAME = 'inventori-cache-v2';
 
 const urlsToCache = [
-    '/',
+
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+        )
     );
 });
 
 self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+        return;
+    }
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
     );
 });
